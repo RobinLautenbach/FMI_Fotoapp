b8857e202eda44694c00f2f8ce8ca1c8c7bb1083
Unknown <christophcaps@gmail.com>
2017-10-15 20:25:25 +0200

    insert speechrecognation in main activity

--- app/src/main/java/com/example/robin/fmi_fotoapp/MainActivity.java
+++ app/src/main/java/com/example/robin/fmi_fotoapp/MainActivity.java
@@ -61,6 +61,18 @@ import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 
+// Imports for Speech Recognizer
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+
+import edu.cmu.pocketsphinx.Assets;
+import edu.cmu.pocketsphinx.Hypothesis;
+import edu.cmu.pocketsphinx.SpeechRecognizerSetup;
+
 /**
  * Created by Robin
  */
@@ -93,6 +105,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     //related to speech recognation
     private SpeechRecognizerManager mSpeechRecognizerManager;
+
+    /* Named searches allow to quickly reconfigure the decoder */
+    private static final String KWS_SEARCH = "wakeup";
+    /* Keyword we are looking for to activate menu */
+    private static final String KEYPHRASE = "camera go";
+    private edu.cmu.pocketsphinx.SpeechRecognizer mPocketSphinxRecognizer;
+    private static final String TAG = SpeechRecognizerManager.class.getSimpleName();
 
     //related to shake detection
     private SensorManager sensorManager; //used to read sensors
@@ -153,6 +172,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             cBackgroundHandler.post(new ImageSaver(imageReader.acquireLatestImage())); //save image from background thread
         }
     };
+
+
+
+
+
+
 
     private class ImageSaver implements Runnable{ //used to save images
 
@@ -822,5 +847,133 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         super.onSaveInstanceState(bundle);
         bundle.putString("cId", cId);
     }
+
+
+
+
+
+
+
+    // Speech Recognizer
+
+    public class SpeechRecognizerManager {
+
+        private Context mContext;
+
+        public SpeechRecognizerManager(Context context) {
+            this.mContext = context;
+
+            initPockerSphinx();
+        }
+
+        private void initPockerSphinx() {
+
+            new AsyncTask<Void, Void, Exception>() {
+                @Override
+                protected Exception doInBackground(Void... params) {
+                    try {
+                        Assets assets = new Assets(mContext);
+
+                        //Performs the synchronization of assets in the application and external storage
+                        File assetDir = assets.syncAssets();
+
+                        //Creates a new speech recognizer builder with default configuration
+                        SpeechRecognizerSetup speechRecognizerSetup = SpeechRecognizerSetup.defaultSetup();
+
+                        speechRecognizerSetup.setAcousticModel(new File(assetDir, "en-us-ptm"));
+                        speechRecognizerSetup.setDictionary(new File(assetDir, "cmudict-en-us.dict"));
+
+                        // To disable logging of raw audio comment out this call (takes a lot of space on the device)
+                        //    speechRecognizerSetup.setRawLogDir(assetDir)
+
+                        // Threshold to tune for keyphrase to balance between false alarms and misses
+                        speechRecognizerSetup.setKeywordThreshold(1e-45f);
+
+                        // Use context-independent phonetic search, context-dependent is too slow for mobile
+                        speechRecognizerSetup.setBoolean("-allphone_ci", true);
+
+                        //Creates a new SpeechRecognizer object based on previous set up.
+                        mPocketSphinxRecognizer = speechRecognizerSetup.getRecognizer();
+
+                        // Add Listener
+                        mPocketSphinxRecognizer.addListener(new PocketSphinxRecognitionListener());
+
+                        // Create keyword-activation search.
+                        mPocketSphinxRecognizer.addKeyphraseSearch(KWS_SEARCH, KEYPHRASE);
+
+
+                    } catch (IOException e) {
+                        return e;
+                    }
+                    return null;
+                }
+
+                @Override
+                protected void onPostExecute(Exception result) {
+                    if (result != null) {
+                        Toast.makeText(mContext, "Failed to init pocketSphinxRecognizer ", Toast.LENGTH_SHORT).show();
+                    } else {
+                        switchSearch(KWS_SEARCH);
+                    }
+                }
+            }.execute();
+
+        }
+
+        private void switchSearch(String searchName) {
+            mPocketSphinxRecognizer.stop();
+
+            if (searchName.equals(KWS_SEARCH))
+                mPocketSphinxRecognizer.startListening(searchName);
+
+        }
+
+        protected class PocketSphinxRecognitionListener implements edu.cmu.pocketsphinx.RecognitionListener {
+
+            @Override
+            public void onBeginningOfSpeech() {
+            }
+
+            /**
+             * In partial result we get quick updates about current hypothesis. In
+             * keyword spotting mode we can react here, in other modes we need to wait
+             * for final result in onResult.
+             */
+            @Override
+            public void onPartialResult(Hypothesis hypothesis) {
+                if (hypothesis == null)
+                    return;
+
+
+                String text = hypothesis.getHypstr();
+                if (text.equals(KEYPHRASE)) {
+                    Toast.makeText(mContext, "You said:" + text, Toast.LENGTH_SHORT).show();
+                    takePhoto();
+                    switchSearch(KWS_SEARCH);
+                }
+            }
+
+            @Override
+            public void onResult(Hypothesis hypothesis) {
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+            }
+
+            public void onError(Exception error) {
+            }
+
+            @Override
+            public void onTimeout() {
+            }
+        }
+    }
+
+
+
+
+
+
 
 }
